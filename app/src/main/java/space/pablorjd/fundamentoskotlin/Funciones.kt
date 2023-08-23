a2d9758223b535bfa7133293e64a5f32c8513992
pablorjd1 <pablo.jimenez2221@gmail.com>
2023-08-23 11:58:03 -0400

    funcion infix

--- app/src/main/java/space/pablorjd/fundamentoskotlin/Funciones.kt
+++ app/src/main/java/space/pablorjd/fundamentoskotlin/Funciones.kt
@@ -1,10 +1,15 @@
 package space.pablorjd.fundamentoskotlin
 
+import kotlin.math.abs
+
 fun main () {
     sayHello()
     val result = sum(1,2)
 
     println(result)
+    newTopic("Infix function")
+    val c = -3
+    println( c.enableAbs(false) )
 }
 
 // la funcion al hacerla privada solo puede ser invocada en esta clase o archivo .kt
@@ -18,3 +23,6 @@ fun sum(a:Int, b:Int): Int {
 }
 // funcion con retorno escrita de manera abreviada
 fun sub ( a:Int, b:Int ) = a - b
+
+// funciones Infix ==> agrega una nueva funcionalidad a la clase con la que se crea en este caso Int
+infix fun Int.enableAbs( enable: Boolean ) = if (enable) abs(this) else this
