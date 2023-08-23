3b0c50605e9dd1dbc133b93f5acf95ad8e054237
pablorjd1 <pablo.jimenez2221@gmail.com>
2023-08-23 11:26:08 -0400

    variables y const permitir null y o cualquier tipo

--- app/src/main/java/space/pablorjd/fundamentoskotlin/MainKotlin.kt
+++ app/src/main/java/space/pablorjd/fundamentoskotlin/MainKotlin.kt
@@ -10,6 +10,13 @@ fun main() {
     val tipoVariable: Int // a las variables y constantes se les puede asigna un tipo especifico
     println(a)
     println(b)
+
+    var objNull: String? // nuesta variable objetoNull se puede asignar un null colocando el simbolo de pregunta el simil de angular es que es una variable opcional
+
+    objNull = null
+    println(objNull)
+    var cualquirTIpo: Any // al asignar Any la variable puede tener cualquier tipo de dato
+
 }
 
 fun newTopic( topic: String ) {
