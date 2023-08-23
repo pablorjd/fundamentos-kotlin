e94acc0f27e1ed57021b3196558716713bd1cae3
pablorjd1 <pablo.jimenez2221@gmail.com>
2023-08-23 11:21:50 -0400

    variables y constantes

--- app/src/main/java/space/pablorjd/fundamentoskotlin/MainKotlin.kt
+++ app/src/main/java/space/pablorjd/fundamentoskotlin/MainKotlin.kt
@@ -1,5 +1,20 @@
 package space.pablorjd.fundamentoskotlin
 
 fun main() {
-    println("Hola Kotlin")
+    // println("Hola Kotlin")
+    newTopic( "Hola Kotlin" )
+
+    newTopic( "Variable y constantes" )
+    val a = 2 // no pueden mutar
+    var b = "hola soy una variable" // estas pueden cambianr en el flujo de trabajo
+    val tipoVariable: Int // a las variables y constantes se les puede asigna un tipo especifico
+    println(a)
+    println(b)
+}
+
+fun newTopic( topic: String ) {
+    println("""
+         "Este es un nuevo topic"
+        $topic 
+        """.trimIndent())
 }
