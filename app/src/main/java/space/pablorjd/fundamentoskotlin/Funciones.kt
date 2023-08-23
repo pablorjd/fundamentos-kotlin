999f8471006e3fcf97ce7abb1104e64c70e4f2b5
pablorjd1 <pablo.jimenez2221@gmail.com>
2023-08-23 11:51:42 -0400

    funciones con retorno

--- app/src/main/java/space/pablorjd/fundamentoskotlin/Funciones.kt
+++ app/src/main/java/space/pablorjd/fundamentoskotlin/Funciones.kt
@@ -2,11 +2,19 @@ package space.pablorjd.fundamentoskotlin
 
 fun main () {
     sayHello()
+    val result = sum(1,2)
+
+    println(result)
 }
 
 // la funcion al hacerla privada solo puede ser invocada en esta clase o archivo .kt
+// Unit === void => que no retorna ningun tipo de dato la funcion
 private fun sayHello(): Unit {
     println("funciones")
 }
-
-// Unit === void => que no retorna ningun tipo de dato la funcion
+// funcion con retorno escrita de manera normal
+fun sum(a:Int, b:Int): Int {
+    return a + b
+}
+// funcion con retorno escrita de manera abreviada
+fun sub ( a:Int, b:Int ) = a - b
